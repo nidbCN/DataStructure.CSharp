cceadd5f7683ad1c733fb018d9e90ac62c5bbe27
Gaein_nidb <mail@gaein.cn>
2021-08-03 13:00:58 +0800

    Add stack

--- DataNode.cs
+++ DataNode.cs
@@ -2,9 +2,21 @@
 {
     internal class DataNode<T>
     {
-        DataNode<T> Next { get; set; }
-        DataNode<T> Prev { get; set; }
+        public DataNode(T data) : this(data, null)
+        {
 
-        T Data { get; set; }
+        }
+
+        public DataNode(T data, DataNode<T> prev)
+        {
+            Data = data;
+            Prev = prev;
+        }
+
+        public DataNode<T> Next { get; set; }
+        public DataNode<T> Prev { get; set; }
+        public bool HasNext() => !(Next is null);
+        public bool HasPrev() => !(Prev is null);
+        public T Data { get; set; }
     }
 }
--- Stack.cs
+++ Stack.cs
@@ -2,8 +2,23 @@
 
 namespace DataStructure
 {
-    public class Stack
+    public class Stack<T>
     {
+        private DataNode<T> node;
+        public uint Count { get; set; }
 
+        public void Push(T data)
+        {
+            Count++;
+            node = new DataNode<T>(data, node);
+        }
+
+        public T Pop()
+        {
+            if (!node.HasPrev()) throw new IndexOutOfRangeException();
+
+            Count--;
+            return (node = node.Prev).Data;
+        }
     }
 }
