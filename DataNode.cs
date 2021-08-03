5b9bb3a34b768ca423a6d1124021750a3a919ea9
Gaein_nidb <mail@gaein.cn>
2021-08-03 12:48:30 +0800

    init

---
+++ DataNode.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure
+{
+    internal class DataNode<T>
+    {
+        DataNode<T> Next { get; set; }
+        DataNode<T> Prev { get; set; }
+
+        T Data { get; set; }
+    }
+}
