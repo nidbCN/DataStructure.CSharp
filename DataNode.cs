9e594f333e53238a8b00c136daa657e2f1faf650
Gaein_nidb <mail@gaein.cn>
2021-08-23 20:18:38 +0800

    Update to DataNode to allow null

--- DataNode.cs
+++ DataNode.cs
@@ -2,21 +2,38 @@
 {
     internal class DataNode<T>
     {
-        public DataNode(T data) : this(data, null)
-        {
+#nullable enable
+        /// <summary>
+        /// New a DataNode with value data.
+        /// </summary>
+        /// <param name="data">value</param>
+        public DataNode(T data) : this(data, null) { }
 
-        }
-
-        public DataNode(T data, DataNode<T> prev)
+        /// <summary>
+        /// New a DataNode with value data and previous node.
+        /// </summary>
+        /// <remarks>
+        /// If prev is not null, this method will link prev node with this new node.
+        /// </remarks>
+        /// <param name="data">value</param>
+        /// <param name="prev">previous node</param>
+        public DataNode(T data, DataNode<T>? prev)
         {
             Data = data;
-            Prev = prev;
+
+            if (prev is not null)
+            {
+                Prev = prev;
+                prev.Next = this;
+            }
         }
 
-        public DataNode<T> Next { get; set; }
-        public DataNode<T> Prev { get; set; }
+        public T Data { get; set; }
+
+        public DataNode<T>? Next { get; set; }
+        public DataNode<T>? Prev { get; set; }
         public bool HasNext() => !(Next is null);
         public bool HasPrev() => !(Prev is null);
-        public T Data { get; set; }
+
     }
 }
