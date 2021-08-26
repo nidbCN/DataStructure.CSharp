b8b87b297a27139dde04ca4473a375e0f0e4154d
Gaein_nidb <mail@gaein.cn>
2021-08-26 21:09:02 +0800

    Add find and indexOf method

--- LinkedList.cs
+++ LinkedList.cs
@@ -5,14 +5,26 @@ namespace DataStructure
 
     public class LinkedList<T> : IEnumerable<T>, ICollection<T>, ICollection
     {
+        /// <summary>
+        /// The number of elements contained in the LinkedList<T>.
+        /// </summary>
         public int Count => _count;
 
+        /// <summary>
+        /// The first node of LinkedList.
+        /// </summary>
         public DataNode<T>? Head => _head;
+
+        /// <summary>
+        /// The last node of LinkedList.
+        /// </summary>
         public DataNode<T>? Tail => _tail;
+
 
         private int _count;
         private DataNode<T>? _head;
         private DataNode<T>? _tail;
+
 
         public bool IsReadOnly => false;
 
@@ -21,12 +33,8 @@ namespace DataStructure
 
         public object SyncRoot => throw new NotImplementedException();
 
-        /// <summary>
-        /// Insert a new node at the end of LinkedList.
-        /// </summary>
-        /// <param name="val">value</param>
-        public void Add(T val)
-            => _head ??= (_tail = new(val, next: _tail));
+        void ICollection<T>.Add(T val)
+            => AddFirst(val);
 
         /// <summary>
         /// Insert a new node at the head of LinkedList.
@@ -35,6 +43,9 @@ namespace DataStructure
         public void AddFirst(T val)
             => _tail ??= (_head = new(val, next: _head));
 
+        public void AddLast(T val)
+            => _head ??= (_tail = new(val, next: _tail));
+
         public void Clear()
         {
             _head = _tail = null;
@@ -42,7 +53,7 @@ namespace DataStructure
         }
 
         public bool Contains(T val)
-            => Find(x => x?.Equals(val) ?? false) is not null;
+            => Find(val) is not null;
 
         public void CopyTo(T[] array, int arrayIndex)
         {
@@ -59,8 +70,30 @@ namespace DataStructure
         }
 
         public DataNode<T>? Find(T? item)
-        {
+            => Find(x => EqualityComparer<T>.Default.Equals(item, x));
 
+
+        public DataNode<T>? Find(Predicate<T> match)
+        {
+            if (match is null) throw new ArgumentNullException(nameof(match));
+            if (_head is null) return default;
+
+            (var result, _) = FindNode(x => match(x.Data));
+
+            return result;
+        }
+
+        public DataNode<T>? FindLast(T? item)
+            => FindLast(x => EqualityComparer<T>.Default.Equals(item, x));
+
+        public DataNode<T>? FindLast(Predicate<T> match)
+        {
+            if (match is null) throw new ArgumentNullException(nameof(match));
+            if (_tail is null) return default;
+
+            (var result, _) = FindLastNode(x => match(x.Data));
+
+            return result;
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -70,6 +103,8 @@ namespace DataStructure
 
         public bool Remove(T item)
         {
+
+
 
         }
 
@@ -93,26 +128,32 @@ namespace DataStructure
         }
 
         public int IndexOf(T item)
-            => IndexOf(x => x?.Equals(item) ?? false);
+            => IndexOf(x => EqualityComparer<T>.Default.Equals(item, x));
 
         public int IndexOf(Predicate<T> match)
         {
-            var ret = -1;
-            if (_head is null) return ret;
+            if (match is null) throw new ArgumentNullException(nameof(match));
 
-            (_, ret) = FindNode(x => match.Invoke(x.Data));
+            var result = -1;
 
-            return ret;
+            if (_head is null) return result;
+
+            (_, result) = FindNode(x => match.Invoke(x.Data));
+
+            return result;
         }
 
-        public T? Find(Predicate<T> match)
+        public int LastIndexOf(Predicate<T> match)
         {
             if (match is null) throw new ArgumentNullException(nameof(match));
-            if (_head is null) return default;
 
-            (var ret, _) = FindNode(x => match(x.Data));
+            var result = -1;
 
-            return ret is null ? default : ret.Data;
+            if (_tail is null) return result;
+
+            (_, result) = FindLastNode(x => match.Invoke(x.Data));
+
+            return result;
         }
 
         private (DataNode<T>?, int) FindNode(Predicate<DataNode<T>> match)
@@ -120,9 +161,22 @@ namespace DataStructure
             var indexOfList = 0;
 
             for (var pointer = _head; pointer!.HasNext(); pointer = pointer.Next)
-            { 
+            {
                 if (match.Invoke(pointer))
                     return (pointer, indexOfList++);
+            }
+
+            return (null, -1);
+        }
+
+        private (DataNode<T>?, int) FindLastNode(Predicate<DataNode<T>> match)
+        {
+            var indexOfList = Count;
+
+            for (var pointer = _tail; pointer!.HasPrev(); pointer = pointer.Prev)
+            {
+                if (match.Invoke(pointer))
+                    return (pointer, --indexOfList);
             }
 
             return (null, -1);
