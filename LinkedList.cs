368fbb74c7581572762d7e438b05380c4bbb9542
Gaein_nidb <mail@gaein.cn>
2021-08-26 19:57:46 +0800

    Use !. operator

--- LinkedList.cs
+++ LinkedList.cs
@@ -2,18 +2,21 @@
 
 namespace DataStructure
 {
+
     public class LinkedList<T> : IEnumerable<T>, ICollection<T>, ICollection
     {
         public int Count => _count;
-        private int _count;
 
-#nullable enable
+        public DataNode<T>? Head => _head;
+        public DataNode<T>? Tail => _tail;
+
+        private int _count;
         private DataNode<T>? _head;
         private DataNode<T>? _tail;
-#nullable disable
 
         public bool IsReadOnly => false;
 
+        // TODO: Implemente sysnc method.
         public bool IsSynchronized => throw new NotImplementedException();
 
         public object SyncRoot => throw new NotImplementedException();
@@ -22,8 +25,15 @@ namespace DataStructure
         /// Insert a new node at the end of LinkedList.
         /// </summary>
         /// <param name="val">value</param>
-        public void Add(T val) =>
-            _head ??= (_tail = new(val, _tail));
+        public void Add(T val)
+            => _head ??= (_tail = new(val, next: _tail));
+
+        /// <summary>
+        /// Insert a new node at the head of LinkedList.
+        /// </summary>
+        /// <param name="val">value</param>
+        public void AddFirst(T val)
+            => _tail ??= (_head = new(val, next: _head));
 
         public void Clear()
         {
@@ -32,26 +42,25 @@ namespace DataStructure
         }
 
         public bool Contains(T val)
-        {
-            for (var pointer = _head; pointer.HasNext(); pointer = pointer.Next)
-                if (pointer.Data.Equals(val))
-                    return true;
-
-            return false;
-        }
+            => Find(x => x?.Equals(val) ?? false) is not null;
 
         public void CopyTo(T[] array, int arrayIndex)
         {
             var indexOfList = 0;
             var indexOfArray = 0;
 
-            for (var pointer = _head; pointer.HasNext(); pointer = pointer.Next)
+            for (var pointer = _head; pointer!.HasNext(); pointer = pointer.Next)
             {
                 if (indexOfList < arrayIndex)
                     array[indexOfArray++] = pointer.Data;
 
                 indexOfList++;
             }
+        }
+
+        public DataNode<T>? Find(T? item)
+        {
+
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -61,7 +70,7 @@ namespace DataStructure
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -74,7 +83,7 @@ namespace DataStructure
             var indexOfList = 0;
             var indexOfArray = 0;
 
-            for (var pointer = _head; pointer.HasNext(); pointer = pointer.Next)
+            for (var pointer = _head; pointer!.HasNext(); pointer = pointer.Next)
             {
                 if (indexOfList < index)
                     array.SetValue(pointer.Data, indexOfArray);
@@ -84,38 +93,39 @@ namespace DataStructure
         }
 
         public int IndexOf(T item)
+            => IndexOf(x => x?.Equals(item) ?? false);
+
+        public int IndexOf(Predicate<T> match)
         {
-            if (item is null) return -1;
-            if (_head is null) return -1;
+            var ret = -1;
+            if (_head is null) return ret;
 
-            var indexOfList = 0;
+            (_, ret) = FindNode(x => match.Invoke(x.Data));
 
-            for (var pointer = _head; pointer.HasNext(); pointer = pointer.Next)
-            {
-                if (pointer.Data?.Equals(item) ?? false)
-                    return indexOfList;
-
-                indexOfList++;
-            }
-
-            return -1;
+            return ret;
         }
 
-#nullable enable
         public T? Find(Predicate<T> match)
-#nullable restore
         {
             if (match is null) throw new ArgumentNullException(nameof(match));
             if (_head is null) return default;
 
+            (var ret, _) = FindNode(x => match(x.Data));
 
-            for (var pointer = _head; pointer.HasNext(); pointer = pointer.Next)
-            {
-                if (match.Invoke(pointer.Data))
-                    return pointer.Data;
+            return ret is null ? default : ret.Data;
+        }
+
+        private (DataNode<T>?, int) FindNode(Predicate<DataNode<T>> match)
+        {
+            var indexOfList = 0;
+
+            for (var pointer = _head; pointer!.HasNext(); pointer = pointer.Next)
+            { 
+                if (match.Invoke(pointer))
+                    return (pointer, indexOfList++);
             }
 
-            return default;
+            return (null, -1);
         }
     }
 
