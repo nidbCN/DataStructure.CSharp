4c4b57903529513d7e740441796d2c40de12a956
Gaein_nidb <mail@gaein.cn>
2021-09-01 23:21:04 +0800

    Add bin tree node

---
+++ Models/BinTreeNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Models
+{
+    public class BinTreeNode<T> : IDataNode<T>
+    {
+        public T? Data { get; set; }
+
+        public BinTreeNode<T>? LeftChild { get; set; }
+        public BinTreeNode<T>? RightChild { get; set; }
+
+        public bool HasLeftChild => !(LeftChild is null);
+        public bool HasRightChild => !(RightChild is null);
+
+        public BinTreeNode()
+        {
+
+        }
+
+        public BinTreeNode(T? data, BinTreeNode<T>? leftChild = null, BinTreeNode<T>? rightChild = null)
+        {
+            Data = data;
+            LeftChild = leftChild;
+            RightChild = rightChild;
+        }
+    }
+}
